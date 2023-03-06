cc65b75b11232fff948186b055d545807779ad9b
Chika <nyayuchikamsln@gmail.com>
2023-03-06 15:47:31 +0700

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,47 @@
 import streamlit as st
+import numpy as np
+import cv2 as cv
+
+st.title("Klasifikasi Kue Tradisional")
+
+from tensorflow.keras.applications import ResNet50
+
+###Input Image
+uploaded_file = st.file_uploader("Choose a file")
+if uploaded_file is not None:
+    #st.write("nama file yang diupload = ", uploaded_file.name)
+    #ini untuk nampilin gambar, outputnya bytes
+    bytes_image = uploaded_file.read() #baca image
+
+    #nampilin gambar
+    image = Image.open(BytesIO(bytes_image))
+    st.image(image, caption='Data Testing')
+
+    #Preprocessing
+    image_np = np.asarray(image)
+    resized_image = cv.resize(image_np, (200,200))
+    img_rgb = cv.cvtColor(resized_image, cv.COLOR_BGR2RGB)
+    image_rgb = np.asarray(img_rgb)
+
+# Loading ResNet50 model
+base_resnet_model = ResNet50(include_top=False,
+                   input_shape=(200,200,3),
+                   pooling='max',classes=8,
+                   weights='imagenet')
+
+base_resnet_model.trainable = False
+
+# Transfer learning ResNet50
+resnet_model = tf.keras.models.Sequential([
+    base_resnet_model,
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(64, activation="relu"),
+    tf.keras.layers.Dense(64, activation="relu"),
+    tf.keras.layers.Dense(8, activation="softmax")
+])
+
+resnet_model.load_model('hasil_ResNet50.h5')
+
+#Proses
+output = resnet_model.predict(image_rgb)
+st.write(output)
