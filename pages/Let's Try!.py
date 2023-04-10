505e931b6595e99fe0dac2b4bad3ec9cb21dc64e
Chika <nyayuchikamsln@gmail.com>
2023-04-10 14:38:36 +0700

    update design web

--- pages/Let's Try!.py
+++ pages/Let's Try!.py
@@ -8,7 +8,27 @@ from PIL import Image
 from tensorflow.keras.layers import *
 from tensorflow.keras.models import *
 
-st.title("Klasifikasi Kue Tradisional")
+# import streamlit.components.v1 as com
+
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+  background: linear-gradient(to bottom right, #deaaff, #c0fdff);
+}
+
+[data-testid="stHeader"]{
+  background-color: #5e6472;
+}
+
+[data-testid="stSidebar"]{
+  background-color: #5e6472;
+}
+
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+st.title("Indonesia Traditional Cake Classification")
 
 from tensorflow.keras.applications import ResNet50
 
