db0cd81d67f0cc9a10206fb6ad9389b1f6e76cc5
Chika <nyayuchikamsln@gmail.com>
2023-04-23 07:57:35 +0700

    update coding

--- 01_Home.py
+++ 01_Home.py
@@ -1,7 +1,4 @@
 import streamlit as st
-import streamlit.components.v1 as com
-import gmaps
-from ipywidgets import embed
 from PIL import Image
 from streamlit_extras.switch_page_button import switch_page
 from streamlit_extras.mention import mention
@@ -36,7 +33,7 @@ container = st.container()
 with container:
   home1, home2 = st.columns([11,8])
   with home1:
-    st.title("We offer a modern solution for detecting Indonesia traditional food by your picture.")
+    st.title("We offer a modern solution for detecting Indonesia traditional cake by your picture.")
     if st.button("Get Started"):
       switch_page("Let's Try!")
   with home2:
@@ -46,129 +43,170 @@ with container:
         if r.status_code != 200:
             return None
         return r.json()
-    lottie_url = "https://assets5.lottiefiles.com/packages/lf20_dsxct2el.json"
+    lottie_url = "https://assets6.lottiefiles.com/packages/lf20_bpqri9y8.json"
     lottie_json = load_lottieurl(lottie_url)
     st_lottie(lottie_json, height=400)
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
 
 with container:
-  st.subheader("Who We Are?")
-  st.write(
-    """We are participants of batch two of Internship and Independent Study Certified program. Certified internship program
-    is an accelerated program with well-designed learning experiences. Certified independent study is a learning in
-    class that is specially designed and made based on the real challenges faced by partners or industry.
-  """)
+  who1, who2 = st.columns([11,8])
+  with who1:
+    st.subheader("Who We Are?")
+    st.write(
+      """We are participants of batch two of Internship and Independent Study Certified program. Certified internship program
+      is an accelerated program with well-designed learning experiences. Certified independent study is a learning in
+      class that is specially designed and made based on the real challenges faced by partners or industry.
+    """)
+  with who2:
+    st.image(Image.open("img/bg-orbit.jpg"))
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
 
 #Location
 with container:
-  st.header("Location")
-  st.subheader("Orbit Future Academy")
-  st.write("")
-  # Plot coordinates
-  # coordinates = (-6.219179776989336, 106.81404408060594)
-  # _map = gmaps.figure(center=coordinates, zoom_level=12)
-
-  # # Render map in Streamlit
-  # snippet = embed.embed_snippet(views=_map)
-  # html = embed.html_template.format(title=" ", snippet=snippet)
-  # com.html(html, height=500,width=500)
+  st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0;'>Location</h5>", unsafe_allow_html=True)
+  st.markdown("<h2 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0;'>Orbit Future Academy</h2>", unsafe_allow_html=True)
+  loc1, loc2 = st.columns([0.4,10])
+  with loc1:
+    st.image(Image.open("img/loc.png"))
+  with loc2:
+    st.write("""Veteran RI Building 15th Floor Plaza Semanggi, Jl. Jend. Sudirman No.Kav. 50, 
+    RT.1/RW.4, Karet Semanggi, Kecamatan Setiabudi, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 
+    12930""")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
 
 #Class of Cakes
 with container:
-  st.title("Indonesia Traditional Cake")
-  kue1, kue2, kue3 = st.columns(3)
+  st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0;'>Class of</h5>", unsafe_allow_html=True)
+  st.markdown("<h2 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0;'>Indonesia Traditional Cakes</h2>", unsafe_allow_html=True)
+  none, kue1, kue2, kue3, none2 = st.columns(5)
+  with none:
+    st.write(" ")
   with kue1:
+    # st.markdown("<img src:'test/kue_dadar_gulung/5.jpg' style:'display:block; margin-left:auto; margin-right:auto; width:50%;'>", unsafe_allow_html=True)
     image = Image.open("test/kue_dadar_gulung/5.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Dadar Gulung")
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Dadar Gulung</p>", unsafe_allow_html=True)
   with kue2:
     image = Image.open("test/kue_kastengel/0.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Kastengel")
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Kastengel</p>", unsafe_allow_html=True)
   with kue3:
     image = Image.open("test/kue_klepon/4.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Klepon")
-  kue4, logo, kue5 = st.columns(3)
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Klepon</p>", unsafe_allow_html=True)
+  with none2:
+    st.write(" ")
+  none3, kue4, logo, kue5, none4 = st.columns(5)
+  with none3:
+    st.write(" ")
   with kue4:
     image = Image.open("test/kue_lapis/5.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Lapis")
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Lapis</p>", unsafe_allow_html=True)
   with logo:
     image = Image.open("img/client-6.webp")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Cake Finder")
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Cake Finder</p>", unsafe_allow_html=True)
   with kue5:
     image = Image.open("test/kue_lumpur/9.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Lumpur")
-  kue6, kue7, kue8 = st.columns(3)
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Lumpur</p>", unsafe_allow_html=True)
+  with none4:
+    st.write(" ")
+  none5, kue6, kue7, kue8, none6 = st.columns(5)
+  with none5:
+    st.write(" ")
   with kue6:
     image = Image.open("test/kue_putri_salju/0.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Putri Salju")
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Putri Salju</p>", unsafe_allow_html=True)
   with kue7:
     image = Image.open("test/kue_risoles/0.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Risoles")
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Risoles</p>", unsafe_allow_html=True)
   with kue8:
     image = Image.open("test/kue_serabi/12.jpg")
     new_image = image.resize((200,150))
-    st.image(new_image, caption="Kue Serabi")
+    st.image(new_image, use_column_width=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kue Serabi</p>", unsafe_allow_html=True)
+  with none6:
+    st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
 
 #Team
 with container:
-  st.title("Our Team")
+  st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0;'>Team</h5>", unsafe_allow_html=True)
+  st.markdown("<h2 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0;'>Our Team</h2>", unsafe_allow_html=True)
   col1, col2, col3, col4, col5 = st.columns(5)
   with col1:
-    com.html("""
-    <h4 style="font-family:sans-serif; text-align:center;">Athiya Shinta W</h4>
-    <p style="font-family:sans-serif; text-align:center;">Cordoba</p>
-    <p style="font-family:sans-serif; text-align:center;">Universitas Muhammadiyah Surabaya</p>
-    """, height=135)
+    st.markdown("<h5 style='font-family:sans-serif; text-align:center;'>Athiya Shinta W</h5>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Cordoba</p>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center; line-height:20px;'><i>Universitas Muhammadiyah Surabaya</i></p>", unsafe_allow_html=True)
     mention(
       label="@athiyashinta.w",
       url="https://ig.me/m/athiyashinta.w",
     )
   with col2:
-    com.html("""
-    <h4 style="font-family:sans-serif; text-align:center;">Halomoan Filipus S</h4>
-    <p style="font-family:sans-serif; text-align:center;">Jupyter XXI</p>
-    <p style="font-family:sans-serif; text-align:center;">Universitas Telkom</p>
-    """, height=135)
-    mention(
-      label="@moan.fs",
-      url="https://ig.me/m/moan.fs",
-    )
-  with col3:
-    com.html("""
-    <h4 style="font-family:sans-serif; text-align:center;">Nyayu Chika Marselina</h4>
-    <p style="font-family:sans-serif; text-align:center;">Jupyter XXI</p>
-    <p style="font-family:sans-serif; text-align:center;">Universitas Sriwijaya</p>
-    """, height=135)
+    st.markdown("<h5 style='font-family:sans-serif; text-align:center;'>Nyayu Chika M</h5>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Jupyter XXI</p>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center; line-height:20px;'><i>Universitas Sriwijaya</i></p>", unsafe_allow_html=True)
     mention(
       label="@nyayuchika",
       url="https://ig.me/m/nyayuchika",
     )
+  with col3:
+    st.markdown("<h5 style='font-family:sans-serif; text-align:center;'>Halomoan Filipus S</h5>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Jupyter XXI</p>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center; line-height:20px;'><i>Universitas Telkom</i></p>", unsafe_allow_html=True)
+    mention(
+      label="@moan.fs",
+      url="https://ig.me/m/moan.fs",
+    )
   with col4:
-    com.html("""
-    <h4 style="font-family:sans-serif; text-align:center;">Sukma Imelda</h4>
-    <p style="font-family:sans-serif; text-align:center;">Cordoba</p>
-    <p style="font-family:sans-serif; text-align:center;">Universitas Merdeka Malang</p>
-    """, height=135)
+    st.markdown("<h5 style='font-family:sans-serif; text-align:center;'>Sukma Imelda</h5>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Cordoba</p>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center; line-height:20px;'><i>Universitas Merdeka Malang</i></p>", unsafe_allow_html=True)
     mention(
       label="@imld19_",
       url="https://ig.me/m/imld19_",
     )
   with col5:
-    com.html("""
-    <h4 style="font-family:sans-serif; text-align:center;">Wella Novita Andriani</h4>
-    <p style="font-family:sans-serif; text-align:center;">Cordoba</p>
-    <p style="font-family:sans-serif; text-align:center;">Universitas Internasional Semen Indonesia</p>
-    """, height=135)
+    st.markdown("<h5 style='font-family:sans-serif; text-align:center;'>Wella Novita A</h5>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Cordoba</p>" ,unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center; line-height:20px;'><i>Universitas Internasional Semen Indonesia</i></p>", unsafe_allow_html=True)
     mention(
       label="@wellanvtadr",
       url="https://ig.me/m/wellanvtadr",
     )
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
+  st.write(" ")
 
 #Partner
 with container:
