4bc060e596317d5638cc6aa55a798c8f75f67963
Chika <nyayuchikamsln@gmail.com>
2023-04-25 21:33:24 +0700

    update

--- pages/02_About.py
+++ pages/02_About.py
@@ -1,7 +1,9 @@
 import streamlit as st
 from PIL import Image
+import webbrowser
+from streamlit_extras.switch_page_button import switch_page
 
-st.set_page_config(page_title='About Cake Finder', page_icon='img/logo-cakefinder.jpg', layout='wide')
+st.set_page_config(page_title='About Cake Finder', page_icon='img/logo-cakefinder.jpg')
 
 page_bg_img = """
 <style>
@@ -13,6 +15,12 @@ page_bg_img = """
   background-color: #778da9;
 }
 
+.stButton>button {
+  color: #000;
+  border-radius: 10%;
+  backgroud-color: #00ff00;
+}
+
 </style>
 """
 
@@ -22,11 +30,16 @@ container = st.container()
 
 with container:
   st.markdown("<h2 style='font-family:sans-serif; text-align:center; margin-bottom:0;'>What is Cake Finder?</h2>", unsafe_allow_html=True)
-  st.write(
-    """We are participants of batch two of Internship and Independent Study Certified program. Certified internship program
-    is an accelerated program with well-designed learning experiences. Certified independent study is a learning in
-    class that is specially designed and made based on the real challenges faced by partners or industry.
-  """)
+  st.markdown("""
+  <p style='font-family:sans-serif; text-align:justify; margin:0; padding-top:0;'>
+    Cake Finder is a website for classifying Indonesia traditional cakes by image. There are 8 classes in Indonesia
+    traditional cakes in this website such as dadar gulung, kastengel, klepon, lapis, lumpur, putri salju, 
+    risoles, and serabi.
+  </p>"""
+  , unsafe_allow_html=True)
+  st.write(" ")
+  if st.button("Let's Try!"):
+      switch_page("Let's Try!")
   st.write(" ")
   st.write(" ")
   st.write(" ")
@@ -37,35 +50,31 @@ with container:
 with container:
   st.markdown("<h5 style='font-family:sans-serif; text-align:center; margin-bottom:0;'>Class of</h5>", unsafe_allow_html=True)
   st.markdown("<h2 style='font-family:sans-serif; text-align:center; margin:0; padding-top:0;'>Indonesia Traditional Cakes</h2>", unsafe_allow_html=True)
-  none, kue1, kue2, kue3, none2 = st.columns(5)
-  with none:
-    st.write(" ")
+  kue1, kue2, kue3 = st.columns(3)
   with kue1:
     # st.markdown("<img src:'test/kue_dadar_gulung/5.jpg' style:'display:block; margin-left:auto; margin-right:auto; width:50%;'>", unsafe_allow_html=True)
     image = Image.open("test/kue_dadar_gulung/5.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Dadar Gulung</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://en.wikipedia.org/wiki/Dadar_gulung'>Dadar Gulung</a></p>", unsafe_allow_html=True)
+    # if st.button('Read More'):
+    #   webbrowser.open_new_tab('https://id.wikipedia.org/wiki/Dadar_gulung')
   with kue2:
     image = Image.open("test/kue_kastengel/0.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Kastengel</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://en.wikipedia.org/wiki/Kaasstengels'>Kastengel</a></p>", unsafe_allow_html=True)
   with kue3:
     image = Image.open("test/kue_klepon/4.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Klepon</p>", unsafe_allow_html=True)
-  with none2:
-    st.write(" ")
-  none3, kue4, logo, kue5, none4 = st.columns(5)
-  with none3:
-    st.write(" ")
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://en.wikipedia.org/wiki/Klepon'>Klepon</a></p>", unsafe_allow_html=True)
+  kue4, logo, kue5 = st.columns(3)
   with kue4:
-    image = Image.open("test/kue_lapis/5.jpg")
+    image = Image.open("test/kue_lapis/10.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Lapis</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://en.wikipedia.org/wiki/Kue_lapis'>Lapis</a></p>", unsafe_allow_html=True)
   with logo:
     image = Image.open("img/logo-cakefinder.jpg")
     new_image = image.resize((200,150))
@@ -75,26 +84,20 @@ with container:
     image = Image.open("test/kue_lumpur/9.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Lumpur</p>", unsafe_allow_html=True)
-  with none4:
-    st.write(" ")
-  none5, kue6, kue7, kue8, none6 = st.columns(5)
-  with none5:
-    st.write(" ")
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://id.wikipedia.org/wiki/Kue_lumpur'>Lumpur</a></p>", unsafe_allow_html=True)
+  kue6, kue7, kue8 = st.columns(3)
   with kue6:
     image = Image.open("test/kue_putri_salju/0.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Putri Salju</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://en.wikipedia.org/wiki/Kue_putri_salju'>Putri Salju</a></p>", unsafe_allow_html=True)
   with kue7:
     image = Image.open("test/kue_risoles/0.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Risoles</p>", unsafe_allow_html=True)
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://en.wikipedia.org/wiki/Rissole'>Risoles</a></p>", unsafe_allow_html=True)
   with kue8:
     image = Image.open("test/kue_serabi/12.jpg")
     new_image = image.resize((200,150))
     st.image(new_image, use_column_width=True)
-    st.markdown("<p style='font-family:sans-serif; text-align:center;'>Serabi</p>", unsafe_allow_html=True)
-  with none6:
-    st.write(" ")
+    st.markdown("<p style='font-family:sans-serif; text-align:center;'><a href='https://en.wikipedia.org/wiki/Serabi'>Serabi</a></p>", unsafe_allow_html=True)
